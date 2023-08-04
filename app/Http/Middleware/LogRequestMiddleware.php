f1c0e67e44e142df76bfc38af9a3f0046387c901
jsonccs-home <1521910992@qq.com>
2023-08-04 19:42:53 +0800

    4 record all the request information (including the error occurred) to the database, the data structure should contain: unique request ID, request method, request path, request data, response content and time.

---
+++ app/Http/Middleware/LogRequestMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogRequestMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $request->request_id = (string) Str::uuid();
+        $response = $next($request);
+        $logEntry = new RequestLog();
+        $logEntry->request_id = $request->request_id;
+        $logEntry->method = $request->method();
+        $logEntry->path = $request->path();
+        $logEntry->request_data = json_encode($request->all());
+        $logEntry->response_content = $response->getContent();
+        $logEntry->save();
+        return $response;
+    }
+}
