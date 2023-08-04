087fd78c7f4af0d78b302974a395ddddbeed2051
jsonccs-home <1521910992@qq.com>
2023-08-04 19:45:35 +0800

    6. logical test:    	Given a string 's' containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.    	An input string is valid if:    		a. Open brackets must be closed by the same type of brackets.    		b. Open brackets must be closed in the correct order.    		c. Every close bracket has a corresponding open bracket of the same type.    	Example 1:    		Input: s = "()"    		Output: true    	Example 2:

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,39 @@ Route::get('/expected-error', function () {
 Route::get('/unexpected-error', function () {
     throw new Exception('Unexpected Error');
 });
+
+Route::get('/validate-brackets', function (Request $request) {
+    $s = $request->input('s');
+
+    $stack = [];
+    $brackets = [
+        ')' => '(',
+        ']' => '[',
+        '}' => '{',
+    ];
+
+    foreach (str_split($s) as $char) {
+        if (in_array($char, array_values($brackets))) {
+            array_push($stack, $char);
+        } elseif (in_array($char, array_keys($brackets))) {
+            if (empty($stack) || array_pop($stack) !== $brackets[$char]) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Invalid brackets',
+                ], 400);
+            }
+        }
+    }
+
+    if (empty($stack)) {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Valid brackets',
+        ]);
+    } else {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Invalid brackets',
+        ], 400);
+    }
+});
